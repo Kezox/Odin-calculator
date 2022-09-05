256d3feb6bcc2013ba754f763b191f15a03d6ea3
Ilija <ilija.kezovic4869@gmail.com>
2022-09-05 13:51:19 +0200

    Add multiply function and console log

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function subtract(a, b) {
     return a-b
 };
-console.log(subtract(8,5));
+console.log(subtract(8, 5));
+
+
+function multiply(a, b) {
+    return a*b
+};
+console.log(multiply(5, 5));
